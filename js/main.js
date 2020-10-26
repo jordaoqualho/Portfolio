8b2420f8bccdd471ffb4e1ee70682c8de18536e9
jordaoqualho <50970557+jordaoqualho@users.noreply.github.com>
2020-10-26 02:43:10 -0300

    finished the desing

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ menu.addEventListener('click', () => {
   }
 })
 
+//função que ajusta o escroll da tag "a"
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
