4dd353af2dce647c80cfc265ff422a14d0843637
jordaoqualho <jordaoqualho@gmail.com>
2020-11-24 17:43:29 -0300

    finalizando home responsive e arrumando as pastas

--- js/lorena.js
+++
@@ -1,5 +0,0 @@
-$(window).on('load', function () {
-	$('#preloader .progress').delay(1000).fadeOut();
-	$('#preloader').delay(1000).fadeOut('slow'); 
-	$('body').delay(1000).css({'overflow': 'visible'});
-  })
